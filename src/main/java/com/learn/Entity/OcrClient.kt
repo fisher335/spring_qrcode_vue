82d29ccfd610b86fbc18ef72046eb106f121c72e
冯少敏 <fengshaomin@qq.com>
2022-06-09 13:51:32 +0800

    发现是自己弄错了

--- src/main/java/com/learn/Entity/OcrClient.kt
+++ src/main/java/com/learn/Entity/OcrClient.kt
@@ -3,9 +3,11 @@ package com.learn.Entity
 import com.baidu.aip.ocr.AipOcr
 import org.springframework.stereotype.Component
 
+
+//单例模式
 @Component
- class OcrClient : AipOcr("16667601","T3xjr0oIM7Pv6cOBU4N6cn90","IK4bMvckG5yMYEAswKRYeGGFlfQddUlV") {
-    val APP_ID = "16667601"
-    val API_KEY = "T3xjr0oIM7Pv6cOBU4N6cn90"
-    val SECRET_KEY = "IK4bMvckG5yMYEAswKRYeGGFlfQddUlV"
+object OcrClient : AipOcr("16667601","T3xjr0oIM7Pv6cOBU4N6cn90","IK4bMvckG5yMYEAswKRYeGGFlfQddUlV") {
+    const val APP_ID = "16667601"
+    const val API_KEY = "T3xjr0oIM7Pv6cOBU4N6cn90"
+    const val SECRET_KEY = "IK4bMvckG5yMYEAswKRYeGGFlfQddUlV"
 }
